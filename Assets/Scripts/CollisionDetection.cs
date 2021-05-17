22579c91d7ee6d331724671e576eb5fd1cbfcda9
Shane Hyland Pierce <shylandpierce@guidewire.com>
2021-05-17 16:31:02 +0100

    Added Score UI

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        /*if(collision.transform.name != "Ground")
+        {
+            KeepScore.Score += 100;
+        }*/
+        KeepScore.Score += 100;
+    }
+}
