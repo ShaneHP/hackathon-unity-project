87f69fadc0ab2d48bf8c4b213758a0f1c3c89e30
Shane Hyland Pierce <shylandpierce@guidewire.com>
2021-05-17 17:34:22 +0100

    added end screen

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    public Text finalScore;
+    private void OnTriggerEnter(Collider other)
+    {
+        gameManager.CompleteLevel();
+        finalScore.text = "$" + KeepScore.Score.ToString();
+    }
+}
